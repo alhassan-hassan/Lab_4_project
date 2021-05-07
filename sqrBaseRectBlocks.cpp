cf9b7ea8392a450189ffad2255db00d56b1e5a2e
Alhassan Hassan <66450402+alhassan-hassan@users.noreply.github.com>
2021-05-07 00:36:03 +0000

    Update sqrBaseRectBlocks.cpp

--- sqrBaseRectBlocks.cpp
+++ sqrBaseRectBlocks.cpp
@@ -1,15 +1,31 @@
 #include "rectBlocks.h"
+#include <cmath>
+#include <math.h>
 
-sqrBaseRectBlocks :: sqrBaseRectBlocks()
+SqrBaseRectBlocks :: SqrBaseRectBlocks()
 {
     this->width = 0;
     this->length = 0;
     this->height = 0;
 }
 
-sqrBaseRectBlocks::sqrBaseRectBlocks (double width, double length)
+SqrBaseRectBlocks::SqrBaseRectBlocks (double width, double length)
 {
     this->width = width;
     this->height = width;
     this->length = length;
 }
+
+double SqrBaseRectBlocks :: volume()
+{
+    double volume = this->width * this->width * this->length;
+    return volume;
+}
+
+double SqrBaseRectBlocks :: surfaceArea()
+{
+    double surfaceArea = 2* pow(this->width, 2)  + (4 * this->width * this->length);
+    return surfaceArea;
+}
+
+
