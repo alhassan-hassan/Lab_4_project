0c8134f3038d39af23141c93e96c28212555c0bc
Alhassan Hassan <66450402+alhassan-hassan@users.noreply.github.com>
2021-05-07 19:20:56 +0000

    Update rectBlocks.cpp

--- rectBlocks.cpp
+++ rectBlocks.cpp
@@ -1,5 +1,7 @@
+//Loading necessary libraries
 #include "rectBlocks.h"
 
+//Constructor
 CuboidBlocks :: CuboidBlocks()
 {
     this->width = 0;
@@ -7,6 +9,7 @@ CuboidBlocks :: CuboidBlocks()
     this->height = 0;
 }
 
+//Parametric constructor
 CuboidBlocks::CuboidBlocks(double length)
 {
     this->width = length;
@@ -14,12 +17,14 @@ CuboidBlocks::CuboidBlocks(double length)
     this->length = length;
 }
 
+//Volume function
 double CuboidBlocks :: volume()
 {
     double volume = this->width * this->height * this->length;
     return volume;
 }
 
+//Area function
 double CuboidBlocks :: surfaceArea()
 {
     double surfaceArea = 2* (this->width * this->height + this->width * this->length + this->length * this->height  );
