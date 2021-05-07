1cc400f3f8a2d4c91d764349331f09bba064ba65
Alhassan Hassan <66450402+alhassan-hassan@users.noreply.github.com>
2021-05-07 00:32:49 +0000

    Update rectBlocks.cpp

--- rectBlocks.cpp
+++ rectBlocks.cpp
@@ -1,15 +1,28 @@
 #include "rectBlocks.h"
 
-rectBlocks :: rectBlocks()
+CuboidBlocks :: CuboidBlocks()
 {
     this->width = 0;
     this->length = 0;
     this->height = 0;
 }
 
-rectBlocks::rectBlocks(double width, double length, double height)
+CuboidBlocks::CuboidBlocks(double length)
 {
-    this->width = width;
-    this->height = height;
+    this->width = length;
+    this->height = length;
     this->length = length;
 }
+
+double CuboidBlocks :: volume()
+{
+    double volume = this->width * this->height * this->length;
+    return volume;
+}
+
+double CuboidBlocks :: surfaceArea()
+{
+    double surfaceArea = 2* (this->width * this->height + this->width * this->length + this->length * this->height  );
+    return surfaceArea;
+}
+
