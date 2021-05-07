d68a901531d9b91b69e338078bf6a18b7b6bcf5c
Alhassan Hassan <66450402+alhassan-hassan@users.noreply.github.com>
2021-05-07 19:23:45 +0000

    Update sphericalBlocks.cpp

--- sphericalBlocks.cpp
+++ sphericalBlocks.cpp
@@ -1,7 +1,9 @@
+//loading libraries and rectBlocks header file
 #include "rectBlocks.h"
 #include <cmath>
 #include <math.h>
 
+//Constructor
 SphericalBlocks  :: SphericalBlocks ()
 {
     this->width = 0;
@@ -9,6 +11,7 @@ SphericalBlocks  :: SphericalBlocks ()
     this->height = 0;
 }
 
+//Parametric Constructor
 SphericalBlocks :: SphericalBlocks (double length)
 {
     this->width = length;
@@ -16,13 +19,14 @@ SphericalBlocks :: SphericalBlocks (double length)
     this->length = length;
 }
 
-
+//Volume function
 double SphericalBlocks :: volume()
 {
     double volume = 4/3 *  M_PI * pow(this->width/2, 3);
     return volume;
 }
 
+//Area function
 double SphericalBlocks :: surfaceArea()
 {
     double surfaceArea = 4 * M_PI * pow(this->width/2, 2);
