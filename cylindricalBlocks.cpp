2850b84fb95a4aca8e1eb7d034cf882d7bf57020
Alhassan Hassan <66450402+alhassan-hassan@users.noreply.github.com>
2021-05-07 00:33:42 +0000

    Update cylindricalBlocks.cpp

--- cylindricalBlocks.cpp
+++ cylindricalBlocks.cpp
@@ -2,29 +2,29 @@
 #include <cmath>
 #include <math.h>
 
-cylindricalBlocks :: cylindricalBlocks()
+CylindricalBlocks :: CylindricalBlocks()
 {
     this->width = 0;
     this->length = 0;
     this->height = 0;
 }
 
-cylindricalBlocks::cylindricalBlocks (double width, double length)
+CylindricalBlocks::CylindricalBlocks (double width, double length)
 {
     this->width = width;
     this->height = width;
     this->length = length;
 }
 
-double cylindricalBlocks :: volume()
+double CylindricalBlocks :: volume()
 {
-    double volume = 4/3 *  M_PI * pow(this->width/2, 3);
+    double volume = M_PI * pow(this->width/2, 2) * this->length;
     return volume;
 }
 
-double cylindricalBlocks :: surfaceArea()
+double CylindricalBlocks :: surfaceArea()
 {
-    double surfaceArea = 4 * M_PI * pow(this->width/2, 2);
+    double surfaceArea = 2 * (M_PI * pow(this->width/2, 2) * this->length) + (2 * M_PI * this->width/2 * this->length);
     return surfaceArea;
 }
 
